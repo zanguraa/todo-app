49960ba523acf7c03ac049e47b01da8d1a6d8e59
valeri zanguri <zanguraa@gmail.com>
2022-10-01 03:23:37 +0400

    some

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -4,6 +4,7 @@ export default function ItemCount() {
   return (
     <div className="flex justify-between  max-w-[520px] w-full  h-12 px-4  items-center  bg-white rounded-md">
       <p className="text-[#9495A5] text-xs">5 item left</p>
+      
       <p className="text-[#9495A5] text-xs">Clear Completed</p>
     </div>
   );
