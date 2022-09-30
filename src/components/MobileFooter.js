10f119c431b9ea7ce359110545b79f87e7e324be
valeri zanguri <zanguraa@gmail.com>
2022-10-01 02:31:16 +0400

    edited

--- src/components/MobileFooter.js
+++ src/components/MobileFooter.js
@@ -2,7 +2,6 @@ export default function MobileFooter() {
   return (
     <div className="flex items-center mt-5 justify-center max-w-[520px] w-full  h-12 font-bold gap-5 bg-white rounded-md sha-[0px 35px 50px -15px rgba(194, 195, 214, 0.5)]">
       <p className="text-[#9495A5]">All</p>
-      <p>this is a {browser} browser</p>
       <p className="text-[#9495A5]">Active</p>
       <p className="text-[#9495A5]">Completed</p>
     </div>
